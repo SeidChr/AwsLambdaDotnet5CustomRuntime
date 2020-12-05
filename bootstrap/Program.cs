00ce5d32261afaeef2e5f7a0a666d5fe7d6966a8
Christian Seidlitz <seidchr+github@gmail.com>
2020-12-05 15:45:25 +0100

    testing runtime support package

--- bootstrap/Program.cs
+++ bootstrap/Program.cs
@@ -1,20 +1,74 @@
-﻿using System.Linq;
-using System;
+﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 
-var apiEndpoint = Environment.GetEnvironmentVariable("AWS_LAMBDA_RUNTIME_API");
-var client = new HttpClient();
+using Amazon.Lambda.Core;
 
-// http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next
-// http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response
 
-while (true) 
+// using Amazon.Lambda.RuntimeSupport;
+// using Amazon.Lambda.Serialization.SystemTextJson;
+// await PackageLoop();
+
+// static async Task PackageLoop()
+// {
+//     using var handlerWrapper = HandlerWrapper.GetHandlerWrapper(
+//         (Func<Payload, ILambdaContext, string>)FunctionHandler,
+//         new DefaultLambdaJsonSerializer());
+
+//     using var bootstrap = new LambdaBootstrap(handlerWrapper);
+
+//     await bootstrap.RunAsync();
+// }
+
+await ManualLoop();
+
+static async Task ManualLoop() 
 {
-    var request = await client.GetAsync($"http://{apiEndpoint}/2018-06-01/runtime/invocation/next");
-    var requestId = request.Headers.GetValues("Lambda-Runtime-Aws-Request-Id").Single();
-    var responseObject = new {success="true"};
-    await client.PostAsync(
-        $"http://{apiEndpoint}/2018-06-01/runtime/invocation/{requestId}/response", 
-        new StringContent(JsonSerializer.Serialize(responseObject)));
+    // http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next
+    // http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response
+    var apiEndpoint = Environment.GetEnvironmentVariable("AWS_LAMBDA_RUNTIME_API");
+    var client = new HttpClient();
+
+    while (true) 
+    {
+        var request = await client.GetAsync($"http://{apiEndpoint}/2018-06-01/runtime/invocation/next");
+        var requestId = request.Headers.GetValues("Lambda-Runtime-Aws-Request-Id").Single();
+
+        var requestBody = await request.Content.ReadAsStringAsync();
+        var payload = JsonSerializer.Deserialize<Payload>(requestBody);
+        var response = FunctionHandler(payload, null);
+
+        await client.PostAsync(
+            $"http://{apiEndpoint}/2018-06-01/runtime/invocation/{requestId}/response", 
+            new StringContent(JsonSerializer.Serialize(response)));
+    }
 }
+
+/// <summary>
+/// A simple function that takes a string and does a ToUpper
+/// </summary>
+/// <param name="input"></param>
+/// <param name="context"></param>
+/// <returns></returns>
+static string FunctionHandler(Payload input, ILambdaContext context)
+{
+    // DoWork();
+    return input?.key1?.ToUpper();
+}
+
+static int DoWork()
+{
+    var result = 0;
+    
+    for (var i = 0; i< 999999999; i++) 
+
+    {
+        result += i;
+    }
+    
+    return result;
+}
+
+record Payload(string key1, string key2, string key3);
--- build.ps1
+++ build.ps1
@@ -1,29 +1,64 @@
-param([switch]$win, [switch]$container)
+param([string] $suffix = "", [switch]$win, [switch]$container, [switch]$restore)
 
-if ($container) {
-    & docker run -it --rm -v "$($PSScriptRoot):/project" -w "/project" --entrypoint pwsh mcr.microsoft.com/dotnet/sdk:5.0
+if ($restore) {
+    & dotnet restore bootstrap
     return
 }
 
+if ($container) {
+    & docker run -it --rm -v "$($PSScriptRoot):/project" -w "/project" --entrypoint pwsh mcr.microsoft.com/dotnet/sdk:5.0 .\build.ps1 $suffix
+    return
+}
+
+# trimming is risky and not really needed
+# also really a bad idea if this executable shall be deplloyed standalone and load other assemblies ont he fly
 $trimmed = "true"
-$singleFile = "true"
+
+# single file seems to require a little more startup time in the lambda
+$singleFile = "false"
+
+# always release for performance!
 $config = "Release"
 
 $lnx = -not $win
 $runtime = $win ? "win-x64" : "linux-x64"
-$path = "bootstrap/bin/$config/net5.0/$runtime/publish"
 
-Remove-Item $path -Recurse -Force -ErrorAction SilentlyContinue
-& dotnet publish bootstrap --configuration $config -p:RuntimeIdentifier=$runtime /p:PublishTrimmed=$trimmed /p:TrimMode=link /p:PublishReadyToRun=true /p:PublishSingleFile=$singleFile
+$trimmedName = $trimmed -eq "true" ? "_trimmed" : ""
+$singleFileName = $singleFile -eq "true" ? "_singleFile" : ""
+$suffixName = $suffix ? "_$suffix" : ""
+
+$fileName = "package$($trimmedName)$($singleFileName)$($suffixName).zip"
+
+$startLocation = Get-Location;
+$artifactsLocation = Join-Path $startLocation "artifacts"
+$publishLocation = Join-Path $startLocation "bootstrap" "bin" $config "net5.0" $runtime "publish"
+$destinationPath = Join-Path $artifactsLocation $fileName
+
+Remove-Item $destinationPath -Force -ErrorAction SilentlyContinue
+Remove-Item $publishLocation -Recurse -Force -ErrorAction SilentlyContinue
+
+& dotnet publish bootstrap `
+    --configuration $config `
+    /p:RuntimeIdentifier=$runtime `
+    /p:PublishTrimmed=$trimmed `
+    /p:TrimMode=link `
+    /p:PublishReadyToRun=true `
+    /p:PublishSingleFile=$singleFile
 
 if ($lnx) {
-    & Push-Location $path
+    & Push-Location $publishLocation
     try {
         & apt update
         & apt install -y zip
-
+        # make boostrap file executable for the linux system
         & chmod +x bootstrap
-        & zip package.zip bootstrap
+
+        New-Item -ItemType Directory -Path $artifactsLocation -Force
+        
+        & zip $destinationPath *
+
+        # this compression method wont keep the executable flag
+        #Compress-Archive * -DestinationPath $destinationPath
     } finally {
         Pop-Location
     }
